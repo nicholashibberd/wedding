df1cfed037f265fd5cd81a0ae2edb7651fbac07f
nicholashibberd <nicholashibberd@gmail.com>
2014-01-14 05:34:12 +0000

    parallax style

--- static/js/views/app_view.js
+++ static/js/views/app_view.js
@@ -9,16 +9,138 @@ define([
       this.app = app;
       this.model = model;
       this.render();
+      this.parallax();
     },
 
     render: function() {
       this.$el.scrollspy(
         {
           target: '#my-nav',
-          offset: 5
+          offset: 250
         }
       )
+    },
+
+    parallax: function() {
+      var sections = {
+        intro: {
+          id: 'intro',
+        },
+        theDayBefore: {
+          id: 'the-day-before',
+        },
+        gettingToTheChurch: {
+          id: 'getting-to-the-church',
+        },
+        theCeremony: {
+          id: 'the-ceremony',
+        },
+        gettingToTheReception: {
+          id: 'getting-to-the-reception',
+        },
+        gorseHill: {
+          id: 'gorse-hill',
+        },
+        theWeddingBreakfast: {
+          id: 'the-wedding-breakfast',
+        },
+        accommodation: {
+          id: 'accommodation',
+        },
+        giftList: {
+          id: 'gift-list',
+        },
+      }
+
+      var theDayBeforeSection = new Section(sections.theDayBefore);
+      var gettingToTheChurchSection = new Section(sections.gettingToTheChurch);
+      var theCeremonySection = new Section(sections.theCeremony);
+      var gettingToTheReceptionSection = new Section(sections.gettingToTheReception);
+      var gorseHillSection = new Section(sections.gorseHill);
+      var theWeddingBreakfastSection = new Section(sections.theWeddingBreakfast);
+      var accommodationSection = new Section(sections.accommodation);
+      var giftListSection = new Section(sections.giftList);
+
+      var sectionObjects = [
+        theDayBeforeSection, 
+        gettingToTheChurchSection,
+        theCeremonySection,
+        gettingToTheReceptionSection,
+        gorseHillSection,
+        theWeddingBreakfastSection, 
+        accommodationSection, 
+        giftListSection, 
+      ]
+         
+      var $window = $(window); 
+      var velocity = 0.8; 
+      var currentSection = sections.intro;
+      
+      function update() { 
+        var activeElements = $('.scroller-main.active'); 
+        
+        for (var i = 0; i < activeElements.length; i++) {
+          var elem = $(activeElements[i]);
+          var elemTop = elem.offset().top; 
+          var pos = $window.scrollTop(); 
+
+          var elemScroll = pos - elemTop;
+
+          var bgPosition = Math.round(elemScroll * velocity);
+          console.log(bgPosition);
+          elem.css('backgroundPosition', '50% ' + bgPosition + 'px'); 
+        }
+      }; 
+
+      function trackSection() {
+        var scrollTop = $window.scrollTop();
+        var scrollBottom = $window.scrollTop() + $window.height();
+
+        for (var i=0; i < sectionObjects.length; i++) {
+          var section = sectionObjects[i];
+          var elem = section.element;
+          if (isScrolledIntoView(elem)) {
+            elem.addClass('active');
+          }
+          else {
+            section.active = false;
+            elem.removeClass('active');
+          }
+        }
+      }
+
+      function Section(data) {
+        this.id = data.id
+        this.element = $('#' + data.id + '-scroller-main');
+        this.startingHeight = data.startingHeight;
+      }
+
+      Section.prototype.position = function() {
+        return this.height() + this.element.position().top;
+      }
+
+      Section.prototype.height = function() {
+        var val = this.element.css('height').replace("px", "");
+        return parseInt(val);
+      }
+      
+      $window.bind('scroll', update);
+      $window.bind('scroll', trackSection);
+
+      function isScrolledIntoView(elem)
+      {
+        var docViewTop = $(window).scrollTop();
+        var docViewBottom = docViewTop + $(window).height();
+
+        var elemTop = elem.offset().top;
+        var elemBottom = elemTop + $(elem).height();
+        
+        return ((elemBottom <= docViewBottom && elemBottom >= docViewTop) || (elemTop >= docViewTop && elemTop <= docViewBottom));
+      }
     }
+
+    
+    
   });
   
   return AppView;
