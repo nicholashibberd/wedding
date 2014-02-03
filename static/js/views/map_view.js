4b92f2f7a3e8eec2dfa8e725f7e23cd72d989e5a
nicholashibberd <nicholashibberd@gmail.com>
2014-02-03 13:48:46 +0000

    removing transport section

--- static/js/views/map_view.js
+++ static/js/views/map_view.js
@@ -11,20 +11,20 @@ define([
         zoom: 15,
         scrollwheel: false
       };
-      var map = new google.maps.Map(document.getElementById('the-church-map'), allSaintsMapOptions);
+      var map = new google.maps.Map(document.getElementById('getting-to-the-church-map'), allSaintsMapOptions);
       var marker = new google.maps.Marker({
         position: allSaintsLatLng,
         map: map,
         title:"All Saints Church, Sanderstead, Surrey"
       });
-      
+
       var gorseHillLatLng = new google.maps.LatLng(51.301587, -0.597526)
       var gorseHillMapOptions = {
         center: gorseHillLatLng,
         zoom: 12,
         scrollwheel: false
       };
-      var map = new google.maps.Map(document.getElementById('gorse-hill-map'), gorseHillMapOptions);
+      var map = new google.maps.Map(document.getElementById('getting-to-the-reception-map'), gorseHillMapOptions);
       var marker = new google.maps.Marker({
         position: gorseHillLatLng,
         map: map,
@@ -35,4 +35,3 @@ define([
 
   return MapView;
 });
-
